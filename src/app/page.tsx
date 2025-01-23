8444caad99607b5639196c55b365f60957cc7ba7
Hiroko.YAMADA <45808217+HirokoYAMADA@users.noreply.github.com>
2025-01-23 12:56:46 +0900

    不要なファイルの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import ChatBot from "@/_components/chatBot";
 import Image from "next/image";
-import Page from "./api/basic/stream-text-generation/page";
 
 export default function Home() {
   return (<ChatBot />);
